fef00b415ae5b63a9b24b526ecf4f453417526e5
Administrator <591081512@qq.com>
2024-04-27 18:17:31 +0800

    flink 案例提交

---
+++ Flink-learning/src/main/java/com/atguigu/chapter02/StreamWordCount.java
@@ -0,0 +1,48 @@
+package com.atguigu.chapter02;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class StreamWordCount {
+    public static void main(String[] args) throws Exception {
+
+
+        // 1. 创建流式执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 获取外界传入的参数
+//        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+//        String host = parameterTool.get("host");
+//        int port = parameterTool.getInt("port");
+
+        // 2. 读取文本流   D:\tools\netcat-win32-1.12   cmd  nc -l -p 7777
+        DataStreamSource<String> lineDSS = env.socketTextStream("localhost", 7777);
+
+        // 3. 转换数据格式
+        SingleOutputStreamOperator<Tuple2<String, Long>> wordAndOne = lineDSS
+                .flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        out.collect(Tuple2.of(word, 1L));
+                    }
+                }).returns(Types.TUPLE(Types.STRING, Types.LONG)); //当 Lambda 表达式
+
+
+        // 4. 分组
+        KeyedStream<Tuple2<String, Long>, String> wordAndOneKS = wordAndOne
+                .keyBy(t -> t.f0);
+        // 5. 求和
+        SingleOutputStreamOperator<Tuple2<String, Long>> result = wordAndOneKS
+                .sum(1);
+        // 6. 打印
+        result.print();
+        // 7. 执行
+        env.execute();
+    }
+}
